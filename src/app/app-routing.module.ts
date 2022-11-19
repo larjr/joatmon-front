f1d1bba42c2873d3bcf0af1d98b4e2a4ebb8a34f
luisangelorjr <luisangelorjr@gmail.com>
2022-11-18 21:34:35 -0300

    ADD: string component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { StringComponent } from './string/string.component';
 
-const routes: Routes = [
-  { path: '/', component: AppComponent }
+const routes: Routes = [  
+  { path: 'string', component: StringComponent }
 ];
 
 @NgModule({
---
+++ src/app/string/string.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-string',
+  templateUrl: './string.component.html',
+  styleUrls: ['./string.component.scss']
+})
+export class StringComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
